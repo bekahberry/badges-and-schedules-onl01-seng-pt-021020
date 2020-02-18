dc552af16045da8b872f684c4c2979fd3b2c3612
Rebekah Gennick <rebekahgennick92@gmail.com>
2020-02-18 02:15:41 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,14 +3,6 @@ def badge_maker(name)
  end
   
   def batch_badge_creator
-   names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-    badge_message = []
-    message_in_badge = names.size
-    names_taken_out = 0
-  while names_taken_out < message_in_badge
-      puts "Hello, my name is #{names[names_taken_out}"
-      names_taken_out += 1  
-    end
-    end
+  []
     
     def assign_rooms 
