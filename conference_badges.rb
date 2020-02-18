101909d8059801b3b2e49d5c73d124f4187c9538
Rebekah Gennick <rebekahgennick92@gmail.com>
2020-02-18 02:31:21 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,10 +2,12 @@ def badge_maker(name)
   return  "Hello, my name is #{name}."
  end
   
-  def batch_badge_creator
-  names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"].each do |name| 
-    puts 
-    
+  def batch_badge_creator(array)
+  new_array = []
+  array.each do |name|
+    new_array.push("Hello, my name is #{name}.")
+  end
+  return new_array
 end
     
    
