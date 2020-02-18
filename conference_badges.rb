31da6bbcdc5b2579fffda9f73a7e02968238954a
Rebekah Gennick <rebekahgennick92@gmail.com>
2020-02-18 02:37:34 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,5 +10,22 @@ def badge_maker(name)
   return new_array
 end
     
-    
+    def assign_rooms(speakers)
+  assign=[]
+  
+  speakers.each_with_index{|speaker,index|
+  assign.push("Hello, #{speaker}! You'll be assigned to room #{index + 1}!")
+    }
+   return assign
+end 
+
+def printer(attendees)
+  batch_badge_creator(attendees).each do |badge|
+    puts badge
+  end
+  
+  assign_rooms(attendees).each do |badge|
+    puts badge
+  end
+end 
    
